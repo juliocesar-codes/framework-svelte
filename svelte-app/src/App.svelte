6377864dcb936ce7978358ebf888fb705d5e6298
JULIO CESAR SANTANA ALVES <25132583@CFP127ALUNO.LOCAL>
2026-01-30 16:51:20 -0300

    HTML e CSS concluido

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,30 +1,40 @@
 <script>
-	export let name;
+    import Input from "./components/input.svelte";
+
+    const raca = "bulldog"
+
+    import { onMount } from "svelte";
+
+    const endpoint = `https://dog.ceo/api/breed/${raca}/images`;
+
+    let data = [];
+
+    onMount(async function () {
+        const response = await fetch(endpoint);
+        const data = await response.json();
+        console.log(data);
+    });
+
+    
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    <img src="https://i.ibb.co/w1MYVvb/Doogle.png" alt="Doogle" border="0" />
+    <Input />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    main {
+        background-color: f0f0f0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+        width: 100%;
+        gap: 20px;
+    }
+    img {
+        height: fit-content;
+    }
 </style>

4d87c9bd8a2c78d7e373063f224032d087c3ed1a
ROGER RIBEIRO DE OLIVEIRA <25132662@CFP127ALUNO.LOCAL>
2026-02-06 13:09:43 -0300

    finalizado

---
+++ svelte-app/src/components/+card.svelte
@@ -0,0 +1,47 @@
+<script>
+  export let src
+
+</script>
+
+<div class="card">
+  <img src={src} alt="Dog"/>
+</div>
+
+<style>
+  .card {
+    height: 250px;
+    max-height: 1000px;
+    border-radius: 20px;
+    overflow: hidden;
+    display: flex;
+    flex-grow: 1;
+    background: rgba(255, 255, 255, 0.2);
+    border-radius: 16px;
+    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+    backdrop-filter: blur(8px);
+    border: 1px solid rgba(255, 255, 255, 0.3);
+    transition:
+      transform 0.3s ease,
+      box-shadow 0.3s ease;
+    font-family: sans-serif;
+    
+    
+  }
+  .card img{
+    width: 100%;
+    height: 100%;
+    max-width: 1000px;
+    max-height: 600px;
+    object-fit: cover;
+    transition: 1s ease;
+    filter: brightness(0.8);
+  }
+  .card img:hover{
+    filter: brightness(1);
+  }
+  .card:hover {
+    transform: translateY(-5px);
+    box-shadow: 0 8px 40px rgba(0, 0, 0, 0.15);
+    
+  }
+</style>
---
+++ svelte-app/src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+    import Input from "../components/+input.svelte";
+    import Card from '../components/+card.svelte';
+
+
+    import doogleImg from "../public/Doogle.png"
+    
+</script>
+
+<main>
+    <img src={doogleImg} alt="Doogle"/>
+    <Input/>
+</main>
+
+<style>
+    main {
+        background-color: f0f0f0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+        width: 100%;
+        gap: 5%;
+    }
+    img {
+        height: fit-content;
+    }
+</style>
